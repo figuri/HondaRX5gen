1a8b6be612d107aca0244748eb1f05db0b20a4ba
unknown <samtisme@gmail.com>
2023-07-20 15:41:29 -0700

    added files and psuedocode

---
+++ index.js
@@ -0,0 +1,33 @@
+// TODO: Include packages needed for this application
+
+// add package that would import the generateMarkdown.js file
+// const generateMarkdown = require('./utils/generateMarkdown.js');
+// export from "badmath"
+// to import we will use require keyword ex: const badmath = require('./badmath.js'); 
+// > use your own word to name the variable (not matching the 'badmath' name)
+// node index.js ; to run the file
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    // create the readme
+
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    // ask the user questions - array of questions []
+    // we need to store the answers as one object {}
+    // we need to generate the markdown 
+    // > calling generate markdown function
+    // > insert object of answers
+    // invoke the writeToFile function
+}
+
+// then we need to call the init function
+
+init();
+
+// var result = init();
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,24 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+`;
+// inject the stuff (can use markdown language)
+// use the backticks to inject the data
+// use the ${} to inject the data (data.title) for example: # ${data.title} 
+
+}
+
+module.exports = generateMarkdown;
